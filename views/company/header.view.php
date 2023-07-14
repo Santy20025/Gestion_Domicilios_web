644cd6864ea085667204ce2db60ef65ab96ecc30
Santy20025 <Santyborda25@gmail.com>
2023-07-14 16:51:11 -0500

    Carpeta Proyecto

---
+++ views/company/header.view.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administrador</title>
+    <link rel="stylesheet" href="assets/css/styles_dashboard.css">
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <div class="borde logo">Logo</div>
+            <div class="borde navegacion">
+                <div class="borde home">
+                    <a href="">Inicio</a>
+                </div>
+                <div class="borde navega">
+                    <a href="">Navegación</a>
+                </div>
+            </div>
+            <div class="borde ingreso">
+                <a href="">Cerrar Sesión</a>
+            </div>
+        </nav>
+        <section>
+            <aside class="borde">
+                <div class="modulo">
+                    <h1>USUARIOS</h1>
+                    <a href="">Crear Usuario</a>
+                    <a href="">Consultar Usuarios</a>
+                </div>
+            </aside>
+            <main class="borde">
+                <div class="borde migas">Migas</div>
+                    <a href="">Inicio</a> / <a href="">...</a>
+                <div class="borde principal">
---
+++ views/company/login.view.php
@@ -0,0 +1,19 @@
+<section class="borde">
+    <form action="" method="post" class="borde">
+        <div class="form-header">
+            <h2>Inicio de Sesion</h2>
+        </div>
+        <div class="form-body">
+        <div class="form-clas"></div>
+            <label for="">Usuario</label>
+            <input type="text" name="user" placeholder="Usuario">
+        </div>
+        <div class="form-control">
+                <label for="">Contraseña</label>
+                <input type="text" name="pass" placeholder="Contraseña">
+            </div>
+        <div class="form-footer">
+            <input type="submit" value="Enviar">
+        </div>
+    </form>
+</section>
